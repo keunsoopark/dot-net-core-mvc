d9de57dd48f68e0e023904feb28ae806cee71f7c
Keunsoo Park <keunsoo.park@arundo.com>
2020-02-15 17:36:59 +0100

    basic C# app

---
+++ myApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+            numberExample();
+            numberDivide();
+            fixedPointTypes();
+            numberPiArea();
+        }
+
+        static void numberExample()
+        {
+            int a = 18;
+            int b = 6;
+            int c = 1 + b;
+            Console.WriteLine(c);
+        }
+
+        static void numberDivide()
+        {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+        }
+
+        static void fixedPointTypes()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+        }
+
+        static void numberPiArea()
+        {
+            double radius = 2.50;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+        }
+    }
+}
